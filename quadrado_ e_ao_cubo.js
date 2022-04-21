730aa3a9b1ab6ed3c14888fa07ec58dea372e285
SAULO NASCIMENTTO <103774455+Saul80@users.noreply.github.com>
2022-04-21 17:33:57 -0300

    Update quadrado_ e_ao_cubo.js

--- quadrado_ e_ao_cubo.js
+++ quadrado_ e_ao_cubo.js
@@ -1,8 +1,9 @@
-Desafio
-Você terá o desafio de escrever um programa que leia um valor inteiro N (1 < N < 1000). Este N é a quantidade de linhas de saída que serão apresentadas na execução do programa.
+// Quadrado e ao Cubo
 
-Entrada
+/* Você terá o desafio de escrever um programa que leia um valor inteiro N (1 < N < 1000). Este N é a quantidade de linhas de saída que serão apresentadas na execução do programa.
+
+- Entrada
 O arquivo de entrada contém um número inteiro positivo N.
 
-Saída
+- Saída
 Imprima a saída conforme o exemplo fornecido.
