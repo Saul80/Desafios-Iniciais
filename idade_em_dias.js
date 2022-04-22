9fe9c16d8d96cd79129c78b0e48f15bc166de8d9
SAULO NASCIMENTTO <103774455+Saul80@users.noreply.github.com>
2022-04-21 23:53:55 -0300

    Delete idade_em_dias.js

--- idade_em_dias.js
+++
@@ -1,8 +0,0 @@
-let totalDeDias = parseInt(gets());
-let meses = parseInt((totalDeDias%365) / 30);
-let anos = parseInt(totalDeDias / 365);
-let dias = parseInt((totalDeDias%365)%30);
-
-console.log(anos + " ano(s)");
-console.log(meses + " mes(es)");
-console.log(dias + " dia(s)");
