e731ede3539d6e6dfdf4a4080e56a67bcdd84da1
SAULO NASCIMENTTO <103774455+Saul80@users.noreply.github.com>
2022-04-22 12:50:09 -0300

    Create solução_ultrapassando_V.js

---
+++ solução_ultrapassando_V.js
@@ -0,0 +1,22 @@
+let R = parseInt(gets());
+
+  let V = 0;
+
+  let adicao = 0;
+  let contagem = 0;
+
+    while(V <= R) {
+ 
+     V = parseInt(gets());
+
+  }
+
+    while(adicao <= V) {
+  
+    adicao += R;
+    R++;
+    contagem++;
+
+  }
+
+    console.log(contagem);
