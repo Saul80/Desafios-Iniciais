3c77254e32b437a4655bdec27a8faf85da0d80f4
SAULO NASCIMENTTO <103774455+Saul80@users.noreply.github.com>
2022-04-27 13:38:32 -0300

    Create idades.java

---
+++ idades.java
@@ -0,0 +1,34 @@
+// Idade
+
+/* Faça um algoritmo para ler um número indeterminado de dados, contendo cada um, a idade de um indivíduo. O último dado, que não entrará nos cálculos, contém o valor de idade negativa. Calcular e imprimir a idade média deste grupo de indivíduos.
+
+- Entrada
+
+A entrada contém um número indeterminado de inteiros. A entrada será encerrada quando um valor negativo for lido.
+
+- Saída
+
+A saída contém um valor correspondente à média de idade dos indivíduos.
+
+A média deve ser impressa com dois dígitos após o ponto decimal.
+*/
+import java.io.IOException;
+import java.util.Scanner;
+
+public class  Desafio {
+	
+    public static void main(String[] args) throws IOException {
+        Scanner leitor = new Scanner(System.in);
+        int cont = 0;
+        //complete o codigo
+        while (      ) {
+            n = leitor.nextInt();
+            if (     ) { 
+                //complete o codigo
+            }
+        }
+        double media = soma / cont;
+        System.out.println(String.format("%.2f", media));
+    }
+	
+}
