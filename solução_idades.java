4304fabdf87eb32ef0b7fd669a24da8b424cd896
SAULO NASCIMENTTO <103774455+Saul80@users.noreply.github.com>
2022-04-27 13:36:19 -0300

    Create solução_idades.java

---
+++ solução_idades.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Desafio {
+	
+  public static void main(String[] args) throws IOException {
+      Scanner leitor = new Scanner(System.in);
+     
+    int cont = 0;
+    double soma = 0.0;
+    int n = leitor.nextInt();
+    while (n > 0) {
+         soma += n;
+         cont++;
+         n = leitor.nextInt();
+    }
+    double media = soma / cont;
+    System.out.println(String.format("%.2f", media));
+  }
+	
+}
