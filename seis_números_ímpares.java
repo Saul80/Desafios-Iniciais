09e1e30cb1cb440913abc3687ae98bc61c6b2fdd
SAULO NASCIMENTTO <103774455+Saul80@users.noreply.github.com>
2022-04-28 08:33:16 -0300

    Create seis_números_ímpares.java

---
+++ seis_números_ímpares.java
@@ -0,0 +1,33 @@
+// Seis Números Pares
+
+/*
+Leia um valor inteiro X. Em seguida apresente os 6 valores ímpares consecutivos
+a partir de X, um valor por linha, inclusive o X se for o caso.
+- Entrada
+A entrada será um valor inteiro positivo.
+- Saída
+A saída será uma sequência de seis números ímpares.
+*/
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Desafio {
+	
+    public static void main(String[] args) throws IOException {
+        Scanner leitor = new Scanner(System.in);
+        int x = leitor.nextInt();
+        int cont = 0;
+
+//continue a solucao de acordo com o solicitado no enunciado
+
+        while (cont <     ) {
+        	if (         ) {
+        		System.out.println(      );
+        		cont   ;
+        	}
+
+        }
+    }
+	
+}
